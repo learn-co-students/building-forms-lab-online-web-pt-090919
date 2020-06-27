08096bc18e556114b1eb558a2fd74e3d02d9944a
github username <you@example.com>
2020-06-27 09:58:13 -0400

    Done.

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import BandInput from '../components/BandInput.js'
+import { connect } from 'react-redux'
 
 class BandsContainer extends Component {
+  addBand = (e, state) => {
+    e.preventDefault()
+    this.props.add(state.name)
+  }
+
+  mapBands = () => {
+    return this.props.bands.map(band => <li>{band.name}</li>)
+  }
+
   render() {
-    return(
-      <div>
-        BandsContainer
-      </div>
-    )
+    return(	    
+      <div>	 
+        <div>
+          <BandInput addBand={this.addBand} />
+        </div>
+        <div>
+          <br />BANDS<br />
+          {this.mapBands()}
+        </div>     
+        </div>
+    )	    
+  }	  
+}	
+
+const mapDispatchToProps = dispatch => {
+  return {
+    add: (band) => {
+      return dispatch({ type: "ADD_BAND", band: band })
+    }
   }
 }
 
-export default BandsContainer
+
+const mapStateToProps = state => {
+  return {bands: state.bands}
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer); 
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,5 +1,13 @@
 export default function manageBand(state = {
   bands: [],
 }, action) {
+  switch (action.type) {
+    case 'ADD_BAND':
+      const newState = {bands: [...state.bands, {name: action.band}]}
+      return newState;
+  default: 
   return state
+ }
 };
+
+
